d849d862248e6a3aa7fbd79e28e41632911f4054
Dustin Van Buskirk <dvb@octopus.com>
2025-04-22 16:43:40 -0700

    update vote b to ghouls

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import json
 import logging
 
 option_a = os.getenv('OPTION_A', "Heroes")
-option_b = os.getenv('OPTION_B', "Goblins")
+option_b = os.getenv('OPTION_B', "Ghouls")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
